ff71d9171eae365b87544574d2986c86c034796b
Jonasz Karwacki <jonaszakr@gmail.com>
2020-05-10 21:50:02 -0700

    update with complete and strings methods

--- Code/prefixtree.py
+++ Code/prefixtree.py
@@ -1,4 +1,5 @@
 #!python3
+from collections import deque
 
 from prefixtreenode import PrefixTreeNode
 
@@ -16,7 +17,8 @@ class PrefixTree:
     tree's root node to a terminal node that marks the end of the string."""
 
     # Constant for the start character stored in the prefix tree's root node
-    START_CHARACTER = ''
+    START_CHARACTER = '^'
+    END_CHARACTER = '$'
 
     def __init__(self, strings=None):
         """Initialize this prefix tree and insert the given strings, if any."""
@@ -24,6 +26,9 @@ class PrefixTree:
         self.root = PrefixTreeNode(PrefixTree.START_CHARACTER)
         # Count the number of strings inserted into the tree
         self.size = 0
+        # Insert terminal character
+        self.insert('')
+        self.size -= 1
         # Insert each string, if any were given
         if strings is not None:
             for string in strings:
@@ -35,46 +40,111 @@ class PrefixTree:
 
     def is_empty(self):
         """Return True if this prefix tree is empty (contains no strings)."""
-        # TODO
+        return self.root.num_children() == 1
 
     def contains(self, string):
         """Return True if this prefix tree contains the given string."""
-        # TODO
+        cur_node = self.root
+
+        for char in string+'$':
+            try:
+                cur_node = cur_node.get_child(char) # Update cur_node
+            except ValueError:
+                ## char wasn't found, so the exact string doesn't exist
+                return False
+        return True
+
 
     def insert(self, string):
         """Insert the given string into this prefix tree."""
-        # TODO
+
+        cur_node = self.root
+
+        for char in string:
+            try:
+                ## Try to update cur_node with existing child for char
+                cur_node = cur_node.get_child(char)
+            except ValueError:
+                ## Child for char doesn't exist, so add new child then update
+                ## cur_child
+                new_node = PrefixTreeNode(char)
+                cur_node.add_child(char, new_node)
+                cur_node = new_node
+
+        try:
+            ## Check if terminate character exists
+            cur_node.get_child('$')
+        except ValueError:
+            ## Terminate character ($) wasn't found, so this must be a new
+            ## string
+            cur_node.add_child('$', PrefixTreeNode('$')) # Terminal node
+            self.size += 1 # Increment because a new string has been inserted
+
 
     def _find_node(self, string):
         """Return a pair containing the deepest node in this prefix tree that
         matches the longest prefix of the given string and the node's depth.
         The depth returned is equal to the number of prefix characters matched.
         Search is done iteratively with a loop starting from the root node."""
-        # Match the empty string
-        if len(string) == 0:
-            return self.root, 0
         # Start with the root node
         node = self.root
-        # TODO
+        depth = 0
+
+        for char in string:
+            try:
+                node = node.get_child(char) # Update node with child
+                depth += 1
+            except ValueError:
+                ## Child node wasn't found. End of prefix has been reached
+                break
+        return node, depth
+
 
     def complete(self, prefix):
         """Return a list of all strings stored in this prefix tree that start
         with the given prefix string."""
         # Create a list of completions in prefix tree
         completions = []
-        # TODO
+        ## BFS Search for all completions (maintain relative order of nodes)
+        node, depth = self._find_node(prefix)
+
+        if depth < len(prefix):
+            return []
+        q = deque([(prefix, node)])
+
+        while len(q):
+            cur_string, node = q.pop()
+
+            for child in node.children:
+
+                if child.character == '$':
+                    ## End of string found
+                    completions.append(cur_string)
+                else:
+                    q.appendleft((cur_string+child.character, child))
+        return completions
 
     def strings(self):
         """Return a list of all strings stored in this prefix tree."""
         # Create a list of all strings in prefix tree
         all_strings = []
-        # TODO
+        q = deque([('', self.root)])
+
+        while len(q):
+            cur_string, node = q.pop()
+
+            for child in node.children:
+                if cur_string != '' and child.character == '$':
+                    all_strings.append(cur_string)
+                else:
+                    q.appendleft((cur_string+child.character, child))
+        return all_strings
 
     def _traverse(self, node, prefix, visit):
         """Traverse this prefix tree with recursive depth-first traversal.
         Start at the given node with the given prefix representing its path in
         this prefix tree and visit each node with the given visit function."""
-        # TODO
+        pass
 
 
 def create_prefix_tree(strings):

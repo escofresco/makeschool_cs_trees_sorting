af94d07662aa4aae70242fc0e13885de49f63e14
Jonasz Karwacki <jonaszakr@gmail.com>
2020-05-10 17:38:37 -0700

    add complexity analysis

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,26 +1,54 @@
 #!python
+from collections import deque
 
+from sorting_iterative import insertion_sort
 
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    Running time: O(n + (max(n) - min(n)))
+    Memory usage: O(n + (max(n) - min(n)))"""
 
+    if len(numbers) < 2:
+        ## Already sorted
+        return numbers
+    min_ = float('inf')
+    max_ = float('-inf')
+
+    for num in numbers:
+        min_ = min(min_, num)
+        max_ = max(max_, num)
+    counts = [0] * (max_ - min_ + 1)
+
+    for number in numbers:
+        counts[number - min_] += 1
+    new_numbers = []
+
+    for i, count in enumerate(counts):
+        new_numbers.extend([i + min_]*count)
+    numbers[:] = new_numbers
 
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    Running time:   O( (n/k)^2 * k) ) = O(n^2 / k)
+                    –> k is the number of buckets
+                    Buckets have size n/k. Insertion sort is quadratic in
+                    the worst cast, therefore each bucket has (n/k)^2 runtime.
+    Memory usage:   O(n)"""
+    buckets = [deque() for _ in range(num_buckets)]
+    smallest = float('inf')
+    largest = float('-inf')
+
+    for number in numbers:
+        smallest = min(smallest, number)
+        largest = max(largest, number)
+
+    for i, number in enumerate(numbers):
+        buckets[number // num_buckets].appendleft(number)
+
+    res = []
+    for bucket in buckets:
+        insertion_sort(bucket)
+        res.extend(bucket)
+    numbers[:] = res
